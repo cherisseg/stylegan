77c6d1d4a41b99d7e0d80a980bcb772bc6b56ec9
cherisseg <73181860+cherisseg@users.noreply.github.com>
2021-04-20 17:01:09 +0200

    Update pretrained_example.py

--- pretrained_example.py
+++ pretrained_example.py
@@ -35,7 +35,7 @@ def main():
     Gs.print_layers()
 
     # Pick latent vector.
-    rnd = np.random.RandomState(1000)
+    rnd = np.random.RandomState(1500)
     latents = rnd.randn(1, Gs.input_shape[1])
 
     # Generate image.
