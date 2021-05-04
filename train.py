dbe3f73db0328516531cd1ecbfd476d38697fe92
cherisseg <73181860+cherisseg@users.noreply.github.com>
2021-05-04 22:59:10 +0200

    Update train.py

--- train.py
+++ train.py
@@ -35,7 +35,8 @@ if 1:
 
     # Dataset.
     #desc += '-chairs_1024';     dataset = EasyDict(tfrecord_dir='chairs_1024', resolution=1024);                 train.mirror_augment = False
-    desc += '-chairs_128';     dataset = EasyDict(tfrecord_dir='chairs_128', resolution=128); train.mirror_augment = False
+    #desc += '-chairs_128';     dataset = EasyDict(tfrecord_dir='chairs_128', resolution=128); train.mirror_augment = False
+    desc += '-chairs_128_edited';     dataset = EasyDict(tfrecord_dir='chairs_128_edited', resolution=128); train.mirror_augment = False
     #desc += '-chairs_256';     dataset = EasyDict(tfrecord_dir='chairs_256', resolution=256); train.mirror_augment = False
     #desc += '-tables_128';     dataset = EasyDict(tfrecord_dir='tables_128', resolution=128); train.mirror_augment = False
     #desc += '-lamps_128';     dataset = EasyDict(tfrecord_dir='lamps_128', resolution=128); train.mirror_augment = False
@@ -64,7 +65,7 @@ if 1:
     sched.D_lrate_dict = EasyDict(sched.G_lrate_dict)
 
     # WGAN-GP loss for CelebA-HQ.
-    desc += '-wgangp'; G_loss = EasyDict(func_name='training.loss.G_wgan'); D_loss = EasyDict(func_name='training.loss.D_wgan_gp'); sched.G_lrate_dict = {k: min(v, 0.002) for k, v in sched.G_lrate_dict.items()}; sched.D_lrate_dict = EasyDict(sched.G_lrate_dict)
+    #desc += '-wgangp'; G_loss = EasyDict(func_name='training.loss.G_wgan'); D_loss = EasyDict(func_name='training.loss.D_wgan_gp'); sched.G_lrate_dict = {k: min(v, 0.002) for k, v in sched.G_lrate_dict.items()}; sched.D_lrate_dict = EasyDict(sched.G_lrate_dict)
 
     # Table 1.
     #desc += '-tuned-baseline'; G.use_styles = False; G.use_pixel_norm = True; G.use_instance_norm = False; G.mapping_layers = 0; G.truncation_psi = None; G.const_input_layer = False; G.style_mixing_prob = 0.0; G.use_noise = False
