611735f010a0066df878fd35ff0a57923f9f05f4
cherisseg <73181860+cherisseg@users.noreply.github.com>
2021-05-09 21:53:14 +0200

    Update training_loop.py

--- training/training_loop.py
+++ training/training_loop.py
@@ -133,9 +133,9 @@ def training_loop(
     network_snapshot_ticks  = 3,       # How often to export network snapshots?
     save_tf_graph           = False,    # Include full TensorFlow computation graph in the tfevents file?
     save_weight_histograms  = False,    # Include weight histograms in the tfevents file?
-    resume_run_id           = 3,     # Run ID or network pkl to resume training from, None = start from scratch.
-    resume_snapshot         = 11609,     # Snapshot index to resume training from, None = autodetect.
-    resume_kimg             = 11609.6,      # Assumed training progress at the beginning. Affects reporting and training schedule.
+    resume_run_id           = 4,     # Run ID or network pkl to resume training from, None = start from scratch.
+    resume_snapshot         = 13590,     # Snapshot index to resume training from, None = autodetect.
+    resume_kimg             = 13590.7,      # Assumed training progress at the beginning. Affects reporting and training schedule.
     resume_time             = 0.0):     # Assumed wallclock time at the beginning. Affects reporting.
 
     # Initialize dnnlib and TensorFlow.
