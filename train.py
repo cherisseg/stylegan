7af53effe1ad6d83176c9e4fea3de3ac2cb1b689
cherisseg <73181860+cherisseg@users.noreply.github.com>
2021-05-10 12:00:59 +0200

    Update train.py

--- train.py
+++ train.py
@@ -61,7 +61,7 @@ if 1:
     # Default options.
     train.total_kimg =15000        #25000
     sched.lod_initial_resolution = 8
-    sched.G_lrate_dict = {128: 0.0015, 256: 0.002, 512: 0.003, 1024: 0.003}
+    sched.G_lrate_dict = {128: 0.001} #{128: 0.0015, 256: 0.002, 512: 0.003, 1024: 0.003}
     sched.D_lrate_dict = EasyDict(sched.G_lrate_dict)
 
     # WGAN-GP loss for CelebA-HQ.
